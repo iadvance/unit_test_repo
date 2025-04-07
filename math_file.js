66ec2ca264490143b8a3141217bdbfadeb185a61
ilya pashin <ilyapashin2514@mail.ru>
2025-04-07 16:10:23 +0200

    good job

--- math_file.js
+++ math_file.js
@@ -1,6 +1,6 @@
-function add (a, b ){
+function add ( a, b ){
     return a + b;
 }
 // added new comment to the code
 // added another comment to the code from github
-module.exports =  {add };
+module.exports =  { add };
