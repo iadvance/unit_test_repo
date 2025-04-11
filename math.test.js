ac87684ee32dd2c71613e84ef1d867d8c4ecd6b3
ilya pashin <ilyapashin2514@mail.ru>
2025-04-11 12:05:34 +0200

    Created test for divide function

--- math.test.js
+++ math.test.js
@@ -3,3 +3,7 @@ const { add } = require ( './math_file' );
 test ( 'add 2 + 3 to equals 5', () => {
     expect (add( 2, 3 )).toBe (5);
 } );
+
+test ( ' divide 100 / 2 to equal 50', () => {
+    expect (returnDivide (100 , 5)).toBe(20);
+});
