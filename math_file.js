f174c3ce729b010a86675cbf60a59c6c672abd0e
ilya pashin <ilyapashin2514@mail.ru>
2025-04-11 12:04:44 +0200

    Created divide function

--- math_file.js
+++ math_file.js
@@ -3,4 +3,9 @@ function add ( a, b ){
 }
 // added new comment to the code
 // added another comment to the code from github
+
+function returnDivide(a,b){
+    return a / b;
+}
+
 module.exports =  { add };
